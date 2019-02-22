ff6ce4875e6c39d4a8bce0bba0a4649824c346fa
Darcy Linde <lindedarcy@gmail.com>
2019-02-22 23:31:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(name_array)
 end
 
 def summon_captain_planet(calls_array)
-  # Your code here
+  calls_array.map {|call| call.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
